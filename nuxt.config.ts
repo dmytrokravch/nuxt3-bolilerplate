388d95f48fb5529df7bb3c2a83a48bdfe9cd9fe6
Dmytro <32672282+dmytrokravch@users.noreply.github.com>
2023-01-29 15:40:07 +0100

    feat: add i18n

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   ssr: true,
   css: ['~/assets/sass/vendor.scss', '~/assets/sass/app.scss'],
-  modules: ['@pinia/nuxt'],
+  modules: ['@pinia/nuxt', '@nuxtjs/i18n'],
   experimental: {
     reactivityTransform: false,
   },
