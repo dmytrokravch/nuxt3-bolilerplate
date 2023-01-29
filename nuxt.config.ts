fbe81726b9353b39ce6a257d57cb03775c1cf837
Dmytro <32672282+dmytrokravch@users.noreply.github.com>
2023-01-29 15:13:59 +0100

    feat: add pinia, basic pinia store

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,7 @@
 export default defineNuxtConfig({
   ssr: true,
   css: ['~/assets/sass/vendor.scss', '~/assets/sass/app.scss'],
+  modules: ['@pinia/nuxt'],
   experimental: {
     reactivityTransform: false,
   },
