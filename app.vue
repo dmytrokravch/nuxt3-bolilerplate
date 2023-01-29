342e40ec84f607fb4d6c5fee82a5223029ae143d
Dmytro <32672282+dmytrokravch@users.noreply.github.com>
2023-01-29 15:19:29 +0100

    feat: add basic seo setup

---
+++ app.vue
@@ -0,0 +1,19 @@
+<script lang="ts">
+export default {
+  setup() {
+    useHead({
+      title: 'Nuxt Boilerplate',
+      titleTemplate: '%s - Nuxt 3',
+      meta: [
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'Nuxt 3 Boilerplate',
+        },
+      ],
+      link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    })
+  },
+}
+</script>
