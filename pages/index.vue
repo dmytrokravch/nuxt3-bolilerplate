fb60f95f6d9b5942388032fd4efead7cbbc438fd
Dmytro <32672282+dmytrokravch@users.noreply.github.com>
2023-03-05 13:19:19 +0100

    feat: home page badge

--- pages/index.vue
+++ pages/index.vue
@@ -7,53 +7,38 @@ definePageMeta({
 <template>
   <div class="w-screen h-screen flex items-center justify-center bg-gray-100">
     <div class="w-full mx-auto py-16">
-      <h1 class="text-3xl text-center font-bold mb-6">Nuxt 3 boilerplate</h1>
       <div
-        class="bg-white px-6 py-4 my-3 w-3/4 mx-auto shadow rounded-md flex items-center"
+        class="sm:w-1/3 w-full mx-auto p-4 text-center bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-info-900 dark:border-gray-700"
       >
-        <div class="w-full text-center mx-auto">
-          <button
-            type="button"
-            class="border border-indigo-500 bg-indigo-500 text-white rounded-md px-4 py-2 m-2 transition duration-500 ease select-none hover:bg-indigo-600 focus:outline-none focus:shadow-outline"
+        <h5 class="mb-2 text-3xl font-bold text-gray-900 dark:text-white">
+          Nuxt 3 boilerplate
+        </h5>
+        <p class="mb-5 text-base text-gray-500 sm:text-lg dark:text-gray-400">
+          Project starter with some useful features
+        </p>
+        <div
+          class="items-center justify-center space-y-4 sm:flex sm:space-y-0 sm:space-x-4"
+        >
+          <a
+            href="https://github.com/dmytrokravch/nuxt3-bolilerplate"
+            target="_blank"
+            class="inline-flex items-center text-blue-600 hover:underline"
           >
-            Primary
-          </button>
-          <button
-            type="button"
-            class="border border-green-500 bg-green-500 text-white rounded-md px-4 py-2 m-2 transition duration-500 ease select-none hover:bg-green-600 focus:outline-none focus:shadow-outline"
-          >
-            Success
-          </button>
-          <button
-            type="button"
-            class="border border-red-500 bg-red-500 text-white rounded-md px-4 py-2 m-2 transition duration-500 ease select-none hover:bg-red-600 focus:outline-none focus:shadow-outline"
-          >
-            Error
-          </button>
-          <button
-            type="button"
-            class="border border-yellow-500 bg-yellow-500 text-white rounded-md px-4 py-2 m-2 transition duration-500 ease select-none hover:bg-yellow-600 focus:outline-none focus:shadow-outline"
-          >
-            Warning
-          </button>
-          <button
-            type="button"
-            class="border border-teal-500 bg-teal-500 text-white rounded-md px-4 py-2 m-2 transition duration-500 ease select-none hover:bg-teal-600 focus:outline-none focus:shadow-outline"
-          >
-            Info
-          </button>
-          <button
-            type="button"
-            class="border border-gray-700 bg-gray-700 text-white rounded-md px-4 py-2 m-2 transition duration-500 ease select-none hover:bg-gray-800 focus:outline-none focus:shadow-outline"
-          >
-            Dark
-          </button>
-          <button
-            type="button"
-            class="border border-gray-200 bg-gray-200 text-gray-700 rounded-md px-4 py-2 m-2 transition duration-500 ease select-none hover:bg-gray-300 focus:outline-none focus:shadow-outline"
-          >
-            Light
-          </button>
+            See the guideline
+            <svg
+              class="w-5 h-5 ml-2"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z"
+              ></path>
+              <path
+                d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z"
+              ></path>
+            </svg>
+          </a>
         </div>
       </div>
     </div>
